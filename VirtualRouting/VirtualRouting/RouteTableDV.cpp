1bdece98d7b87fe026636ad33213ce1f0b2b827e
“iea <112987845@qq.com>
2017-12-31 22:45:40 +0800

    ls and dv modified

--- VirtualRouting/VirtualRouting/RouteTableDV.cpp
+++ VirtualRouting/VirtualRouting/RouteTableDV.cpp
@@ -1,15 +1,102 @@
+#include<vector>
+#include<iostream>
+#include<map>
+#include<queue>
+#include "DataStructure.cpp"
+using namespace std;
 class RouteTableDV
 {
 public:
-    std::vector<routeTableEntry> routetable;
+	vector<routeTableEntry> routetable;//目标，下一跳，cost
+	vector<vector<int>> myTable;
+	map<char, vector<pathInfo>> networkGraph;//只有自己到自己邻居的边
+	vector<Addr> hostAddrs;
+	char myHostName;//本机主机名 
+	RouteTableDV(char name) {
+		myTable = vector<vector<int>>(5, vector<int>(5, 9999));
+		myHostName = name;
+		//获取所有hostAddrs,0对应A的信息
+		hostAddrs.push_back(Addr('A', "172.18.157.159"));
+		hostAddrs.push_back(Addr('B', "172.18.156.76"));
+		hostAddrs.push_back(Addr('C', "172.18.159.66"));
+		hostAddrs.push_back(Addr('D', "172.18.159.150"));
+		hostAddrs.push_back(Addr('E', "172.18.158.165"));
+		//获取到邻居的边,不同主机不一样，去LS那边复制一下就可以了
+		networkGraph['C'] = vector<pathInfo>{
+			pathInfo(hostAddrs[2], hostAddrs[0], 3),
+			pathInfo(hostAddrs[2], hostAddrs['D' - 'A'], 2)
+		};
+		//初始自己的路由表
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
 
-    // 存储网络拓补图的链路信息，char类型为主机的名字（ABCD
-    // 其余为链路信息，对于DV算法来讲，只要存下邻居的路由表信息即可
-    map<char, std::vector<pathInfo>> networkGraph;
+			}
+		}
+		for (int i = 0; i < networkGraph[myHostName].size(); i++) {
+			myTable[myHostName - 'A'][networkGraph[myHostName][i].addr2.name - 'A'] = networkGraph[myHostName][i].cost;//邻居的加进路由，下一跳先不考虑
+			myTable[networkGraph[myHostName][i].addr2.name - 'A'][myHostName - 'A'] = networkGraph[myHostName][i].cost;
+			myTable[i][i] = 0;
+			//routetable.push_back(routeTableEntry(networkGraph[myHostName][i].addr2, networkGraph[myHostName][i].addr2.ipaddress, networkGraph[myHostName][i].cost));
+		}
 
-    RouteTableDV();
-    ~RouteTableDV();
+	}
+	~RouteTableDV();
+	void DValgorithm(/*vector<routeTableEntry> anRouteTable*/vector<vector<int>> anTable, char anHostName) {// 更新路由表，传入的是某个邻居发来的路由表以及它的主机名
 
-    void DValgorithm();          // 更新路由表
-    void getNextHop(Addr dst);   // 获得下一跳路由器
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (anTable[i][j] < myTable[i][j]) myTable[i][j] = anTable[i][j];
+			}
+		}
+		for (int i = 0; i < 5; i++) {//Bellman-Ford算法
+			for (int j = 0; j < 5; j++) {
+				for (int z = 0; z < 5; z++) {
+					if (myTable[i][z] + myTable[z][j] < myTable[i][j]) {
+						myTable[i][j] = myTable[i][z] + myTable[z][j];
+						if (i + 'A' == myHostName) {
+							if (routetable.empty()) {
+								Addr destAddr = Addr(hostAddrs[j]);
+								char nextRoute[16];
+								strcpy(nextRoute, hostAddrs[z].ipaddress);
+								routetable.push_back(routeTableEntry(destAddr, nextRoute, myTable[i][j]));
+							}
+							else {
+								for (auto v : routetable) {
+									if (v.addr == hostAddrs[j]) {
+										strcpy(v.nexthop, hostAddrs[z].ipaddress);
+										v.cost = myTable[i][j];
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+
+
+		/*int neighbor_num = 0;
+		for (int neighbor_num = 0; neighbor_num < routetable.size(); neighbor_num++) {
+		if (routetable[neighbor_num].addr.name == anHostName) {
+		break;
+		}
+		}
+		for (int i = 0; i < anRouteTable.size(); i++) {//算法核心
+		int j = 0;
+		for (j = 0; j < routetable.size(); j++) {
+		if (routetable[j].addr.name == anRouteTable[i].addr.name) {
+		if (routetable[j].cost > anRouteTable[i].cost + routetable[neighbor_num].cost) routetable[j] = routeTableEntry(routetable[j].addr.name, routetable[neighbor_num].nexthop, anRouteTable[i].cost + routetable[neighbor_num].cost);
+		break;
+		}
+		}
+		if (j == routetable.size()) routetable.push_back(routeTableEntry(anRouteTable[i].addr.name, routetable[neighbor_num].nexthop ,anRouteTable[i].cost + routetable[neighbor_num].cost));
+		}*/
+	}
+	char* getNextHop(Addr dst) {   // 获得下一跳路由器
+		for (int i = 0; i < routetable.size(); i++) {
+			if (routetable[i].addr.name == dst.name) return routetable[i].nexthop;
+		}
+		return ("0.0.0.0");//默认返回
+	}
 };
--- VirtualRouting/VirtualRouting/RouteTableLS.cpp
+++ VirtualRouting/VirtualRouting/RouteTableLS.cpp
@@ -1,15 +1,86 @@
+#include<vector>
+#include<iostream>
+#include<map>
+#include<queue>
+#include "DataStructure.cpp"
+using namespace std;
 class RouteTableLS
 {
 public:
-    std::vector<routeTableEntry> routetable;
+	vector<routeTableEntry> routetable;
+	// 存储网络拓补图的链路信息，char类型为主机的名字ABCD
+	map<char, vector<pathInfo>> networkGraph;//整个网络拓扑图的边,networkGraph['A']包括所有从A出发的边
+	vector<Addr> hostAddrs;  //方便主机名和ip地址的转换
+	char myHostName;//本机主机名
+	RouteTableLS(char name) {
+		myHostName = name;
+		//获取所有hostAddrs,0对应A的信息
+		hostAddrs.push_back(Addr('A', "172.18.157.159"));
+		hostAddrs.push_back(Addr('B', "172.18.156.76"));//赋值后是多少位
+		hostAddrs.push_back(Addr('C', "172.18.159.66"));
+		hostAddrs.push_back(Addr('D', "172.18.159.150"));
+		hostAddrs.push_back(Addr('E', "172.18.158.165"));
+		//获取整体拓扑结构,暂时手动输入获取,后期可以尝试动态修改，就是创建了UI之后启动程序让用户输入边数和边权
+		networkGraph['A'] = vector<pathInfo>{
+			pathInfo(hostAddrs[0], hostAddrs['C' - 'A'], 3),
+			pathInfo(hostAddrs[0], hostAddrs['D' - 'A'], 7),
+			pathInfo(hostAddrs[0], hostAddrs['E' - 'A'], 5)
+		};
+		networkGraph['B'] = vector<pathInfo>{
+			pathInfo(hostAddrs[1], hostAddrs['D' - 'A'], 1),
+			pathInfo(hostAddrs[1], hostAddrs['E' - 'A'], 3)
+		};
+		networkGraph['C'] = vector<pathInfo>{
+			pathInfo(hostAddrs[2], hostAddrs[0], 3),
+			pathInfo(hostAddrs[2], hostAddrs['D' - 'A'], 2)
+		};
+		networkGraph['D'] = vector<pathInfo>{
+			pathInfo(hostAddrs[3], hostAddrs[0], 7),
+			pathInfo(hostAddrs[3], hostAddrs['B' - 'A'], 1),
+			pathInfo(hostAddrs[3], hostAddrs['C' - 'A'], 2)
+		};
+		networkGraph['E'] = vector<pathInfo>{
+			pathInfo(hostAddrs[4], hostAddrs[0], 5),
+			pathInfo(hostAddrs[4], hostAddrs['B' - 'A'], 3)
+		};
+		LSalgorithm();//更新路由表
+	}
+	~RouteTableLS();
 
-    // 存储网络拓补图的链路信息，char类型为主机的名字（ABCD
-    // 其余为链路信息，对于LS算法来讲，要存下整个网络拓补图
-    map<char, std::vector<pathInfo>> networkGraph;
-
-    RouteTableLS();
-    ~RouteTableLS();
-
-    void LSalgorithm();          // 更新路由表
-    void getNextHop(Addr dst);   // 获得下一跳路由器
+	void LSalgorithm() {// 更新路由表，01234对应ABCDE,已经在构造函数中调用了，不需要线程中再次调用，DV才要调用
+		routetable.clear();
+		vector<int> dis(hostAddrs.size(), 9999);//各个结点到源的最短距离,9999代表无穷大
+		vector<int> pre(hostAddrs.size(), -1);//前结点
+		vector<bool> isVisit(hostAddrs.size(), false);
+		queue<char> q;
+		q.push(myHostName);
+		dis[myHostName - 'A'] = 0;
+		pre[myHostName - 'A'] = myHostName - 'A';
+		while (!q.empty()) {
+			char temp = q.front();
+			q.pop();
+			for (int i = 0; i < networkGraph[temp].size(); i++) {
+				char add2name = networkGraph[temp][i].addr2.name;
+				if (!isVisit[add2name - 'A']) q.push(add2name);
+				if (networkGraph[temp][i].cost + dis[temp - 'A'] < dis[add2name - 'A']) {
+					dis[add2name - 'A'] = networkGraph[temp][i].cost + dis[temp - 'A'];
+					pre[add2name - 'A'] = temp - 'A';
+				}
+			}
+			isVisit[temp] = true;
+		}
+		for (int i = 0; i < hostAddrs.size(); i++) {//是否需要整条路径信息,应该不需要
+			int getStart = hostAddrs[i].name - 'A';
+			while (pre[getStart] != myHostName - 'A') {//获取下一跳的主机
+				getStart = pre[getStart];
+			}
+			routetable.push_back(routeTableEntry(hostAddrs[i], hostAddrs[getStart].ipaddress, dis[hostAddrs[i].name - 'A']));
+		}
+	}
+	char* getNextHop(Addr dst) {   // 获得下一跳路由器
+		for (int i = 0; i < routetable.size(); i++) {
+			if (routetable[i].addr.name == dst.name) return routetable[i].nexthop;
+		}
+		return ("0.0.0.0");//默认返回
+	}
 };
